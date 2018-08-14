b56e01d19a9639abfb2ccba9f22415bef7cc6a3b
zhangtianyi <zhangtianyi1234@126.com>
2018-08-14 13:43:45 +0800

    add dashboard

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
 
-from api.views import (LocalUrlView,)
+from api.views import (LocalUrlView, DashboardView)
 
 urlpatterns = [
     url(r'^local_url/$', LocalUrlView.as_view(), name='local_url'),
+    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
     url(r'^users/', include('users.urls')),
     url(r'^', include('app.urls')),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import os
+import datetime
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.utils.timezone import make_aware
+from django.db.models import Count, Case, When
+
+from app.models import User, Notes, Sections
 
 
 class LocalUrlView(APIView):
@@ -15,6 +20,50 @@ class LocalUrlView(APIView):
 
         resp_dict = {
             'url': abs_path,
+            'success': True,
+        }
+        return Response(resp_dict, status=status.HTTP_200_OK)
+
+
+def calculate_users_count():
+    users_query = User.objects.all()
+    total_info = {}
+    day_info = users_query.extra({'day': "date(created_at)"}).values('day').annotate(count=Count('pk')).order_by('day')
+    total_info['total'] = users_query.count()
+    total_info['day_info'] = day_info
+    return total_info
+
+
+def calculate_notes_count():
+    query = Notes.objects.all()
+    total_info = {}
+    day_info = query.extra({'day': "date(created_at)"}).values('day').annotate(count=Count('pk')).order_by('day')
+    total_info['total'] = query.count()
+    total_info['day_info'] = day_info
+    return total_info
+
+
+def calculate_sections_count():
+    query = Sections.objects.all()
+    total_info = {}
+    day_info = query.extra({'day': "date(created_at)"}).values('day').annotate(count=Count('pk')).order_by('day')
+    total_info['total'] = query.count()
+    total_info['day_info'] = day_info
+    return total_info
+
+
+class DashboardView(APIView):
+    # permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        end_data = calculate_users_count()
+        notes_data = calculate_notes_count()
+        sections_data = calculate_sections_count()
+
+        resp_dict = {
+            'users': end_data,
+            'notes': notes_data,
+            'sections': sections_data,
             'success': True,
         }
         return Response(resp_dict, status=status.HTTP_200_OK)
